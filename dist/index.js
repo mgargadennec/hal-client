0afc902131b3474b535319c6be153acca992ebd0
mgargadennec <mael.gargadennec@starlingminds.com>
2020-05-14 12:52:37 -0700

    add options

--- dist/index.js
+++ dist/index.js
@@ -4,5 +4,6 @@ function __export(m) {
 }
 Object.defineProperty(exports, "__esModule", { value: true });
 __export(require("./hal-client"));
+__export(require("./hal-client-options"));
 __export(require("./resource"));
 __export(require("./resource-client"));
